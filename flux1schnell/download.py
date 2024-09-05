ab8ec028d188241fff13c3bbf51aa249132454b1
Pann Sengpanich <panncraftyo@gmail.com>
2024-09-05 10:41:39 +0700

    update

--- flux1schnell/download.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="black-forest-labs/FLUX.1-schnell",
-    local_dir="/Users/pannsengpanich/repos/hugging_face_test/flux1schnell",
-)
